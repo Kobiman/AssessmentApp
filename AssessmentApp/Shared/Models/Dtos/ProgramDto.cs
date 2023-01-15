326b367c1aa3a6e27e97c875edf8ddd1ed26a440
Kobina Lawson <kobinalawson@hotmail.com>
2023-01-15 18:01:21 +0000

    gjg

---
+++ AssessmentApp/Shared/Models/Dtos/ProgramDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssessmentApp.Shared.Models.Dtos
+{
+    public class ProgramDto
+    {
+        public string ProgramId { get; set; }
+        public string Name { get; set; }
+    }
+}
